be9e491c2cc2421e9b4d5293649b203c584ef4c0
Vlad <vladtaranov059@gmail.com>
2026-02-08 16:53:58 +0300

    Jenkins

--- test/testAPI.spec.ts
+++ test/testAPI.spec.ts
@@ -6,7 +6,7 @@ test('Check text content on page API', async ({ page }) => {
     process.env['testId'] = 'API';
     const ApiCheck = new Api(page);
     await ApiCheck.goto();
-    await ApiCheck.clickApiButton()
+    await ApiCheck.clickApiButton();
     await ApiCheck.clickApiRequest();
     await ApiCheck.checkText('Exposes API that can be used for the Web API testing.');
 });
